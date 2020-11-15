d4cdc8477c1610fa67f2f3d71594ffd12e5e2ee1
yuezhou777 <zhou.yue2@husky.neu.edu>
2020-11-16 00:54:15 +0800

    modified some files

--- LoginApp/LoginApp/ViewController.swift
+++ LoginApp/LoginApp/ViewController.swift
@@ -52,12 +52,64 @@ class ViewController: UIViewController {
             // I have successfully logged in  go to Dashboard
             self!.performSegue(withIdentifier: "loginSegue", sender: strongSelf)
             
-            
-            
+        }
+        
+        // Add User
+        @IBAction func createUserAccount(_ sender: Any) {
+            showTextInputPrompt(withMessage: "Email:") {  [weak self] userPressedOK, email in
+              guard let strongSelf = self else { return }
+              guard let email = email else {
+                strongSelf.showMessagePrompt("email can't be empty")
+                return
+              }
+              strongSelf.showTextInputPrompt(withMessage: "Password:") { userPressedOK, password in
+                guard let password = password else {
+                  strongSelf.showMessagePrompt("password can't be empty")
+                  return
+                }
+                strongSelf.showSpinner {
+                    
+                  // create user account
+                  Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    strongSelf.hideSpinner {
+                      guard let user = authResult?.user, error == nil else {
+                        strongSelf.showMessagePrompt(error!.localizedDescription)
+                        return
+                      }
+                      print("\(user.email!) created")
+                      strongSelf.navigationController?.popViewController(animated: true)
+                    }
+                  }
+                    
+                }
+              }
+            }
+        }
+        
+        // Reset Email
+        @IBAction func resetPassword(_ sender: Any) {
+          showTextInputPrompt(withMessage: "Email:") { [weak self] userPressedOK, email in
+            guard let strongSelf = self, let email = email else {
+              return
+            }
+            strongSelf.showSpinner {
+                
+              // Send a email reset link
+              Auth.auth().sendPasswordReset(withEmail: email) { error in
+                strongSelf.hideSpinner {
+                  if let error = error {
+                    strongSelf.showMessagePrompt(error.localizedDescription)
+                    return
+                  }
+                  strongSelf.showMessagePrompt("Sent")
+                }
+              }
+                
+            }
+          }
         }
         
     }
-    
 
 }
 
